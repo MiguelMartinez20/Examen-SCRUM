f32c3d4a3a4d91cc333c429c7c23ba43bb725292
MiguelMartinez20 <mig.martinezm@alumnos.duoc.cl>
2019-07-06 23:00:18 -0400

    Inicia Proyecto Agricultores

---
+++ comercio/models.py
@@ -0,0 +1,58 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+class Productor(models.Model):
+
+    ACTIVA = 'Activa'
+    DESHABILITADA = 'Deshabilitada'
+
+    STATE_CHOICES = (
+        (ACTIVA, 'Activa'),
+        (DESHABILITADA, 'Deshabilitada'),
+    )
+
+    rut = models.CharField(max_length=200)
+    nombre = models.CharField(max_length=200)
+    apellido = models.CharField(max_length=200)
+    direccion = models.CharField(max_length=200)
+    latitud = models.CharField(max_length=200)
+    longitud = models.CharField(max_length=200)
+    pyme = models.CharField(max_length=200,blank=True)
+    email = models.CharField(max_length=200)
+    cuenta = models.CharField(max_length=10, choices=STATE_CHOICES, default=ACTIVA)
+
+    def publish(self):
+        self.save()
+
+    def str(self):
+        return self.rut
+
+class Producto(models.Model):
+
+    nombre = models.CharField(max_length=200)
+    detalle = models.CharField(max_length=200)
+    precio = models.CharField(max_length=200)
+    productor = models.ForeignKey(Productor, on_delete=models.CASCADE, blank=True, null=True)
+
+    def publish(self):
+        self.save()
+
+    def str(self):
+        return self.nombre
+
+class Post(models.Model):
+
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def str(self):
+        return self.title  
